afac0432b181333f014afa0348517ea91123f4da
patrcoff-kainos <patrick.coffey@kainos.com>
2023-12-12 15:01:25 +0000

    feat: adds some basic logic

--- pissedman/widget.py
+++ pissedman/widget.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
 import sys
+import requests
 
 from PySide6.QtWidgets import QApplication, QWidget
 
@@ -14,6 +15,16 @@ class Widget(QWidget):
         super().__init__(parent)
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
+        self.method = 'GET' # haven't added this drop down selection yet
+        self.ui.sendBtn.clicked.connect(self.make_request)
+
+    def make_request(self):
+        #body = self.requestEdit.value
+        #response = requests.request(method=self.method, url=self.ui.uriEdit.value,json=body)
+        uri = self.ui.uriEdit.text()
+        response = requests.get(url=uri,verify=False)
+        self.ui.responseViewer.setText(response.text)
+
 
 
 if __name__ == "__main__":
